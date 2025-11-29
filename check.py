c31432b422b34aeadde8b43873c294d63d3fe06a
abcdemi <85865058+abcdemi@users.noreply.github.com>
2025-11-29 14:50:37 +0300

    Update check.py

--- check.py
+++ check.py
@@ -1,2 +1,6 @@
 import torch
 print(f"PyTorch Version: {torch.__version__}")
+print(torch.cuda.is_available())
+
+print("CUDA version:", torch.version.cuda)
+print("Device:", torch.cuda.get_device_name(0) if torch.cuda.is_available() else "No GPU")
